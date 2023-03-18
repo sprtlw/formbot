63248e45e9834ada4992a300d6532fadfa02b2fe
Sam Partlow <sprtlw@pm.me>
2023-03-17 20:44:33 -0400

    fixed script. read it ur damn self lol

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 import random
 import string
+import time
 
 # ask user for number of entries
 num = input("Entries: ")
@@ -15,21 +17,25 @@ with open('names.txt', 'r') as f:
 
 # generate random email
 webEmail = webdriver.Chrome()
-webEmail.get('https://temp-mail.org')
+webEmail.get('https://fakermail.com')
 tEmail = ''
 
 try:
+    # wait for page to load
     element = WebDriverWait(webEmail, 10).until(
-        EC.presence_of_element_located((By.ID, 'mail'))
+        EC.presence_of_element_located((By.ID, 'email-address'))
     )
+    
+    # set tEmail to the value of the element
     tEmail = element.get_attribute('value')
+    
     print(f'Temporary email address: {tEmail}')
-except:
+except TimeoutException:
     print('Temporary email address not generated')
 
 # web init
 web = webdriver.Chrome()
-web.get('https://www.nhl.com/redwings/community/teacher-of-the-month')
+web.get('https://olyent.formstack.com/forms/teacher_of_the_year_2023')
 
 # wait for page to load
 WebDriverWait(web, 10).until(
@@ -40,11 +46,19 @@ WebDriverWait(web, 10).until(
 wFirstName = web.find_element(By.ID, 'field140649479-first')
 wLastName = web.find_element(By.ID, 'field140649479-last')
 wMail = web.find_element(By.ID, 'field140649480')
+wTeach = web.find_element(By.ID, 'field140649482_5')
+wSubmit = web.find_element(By.ID, 'fsSubmitButton5198207')
 
 tempEmailCounter = 0
+sucSub = 0
 
 # send keys
 for i in range(int(num)):
+    # check if page is loaded
+    WebDriverWait(web, 10).until(
+        EC.presence_of_element_located((By.ID, 'field140649479-first'))
+    )
+    
     # send first and last name entries
     wFirstName.send_keys(random.choice(names))
     wLastName.send_keys(random.choice(names))
@@ -63,10 +77,38 @@ for i in range(int(num)):
         print(f'New temporary email address: {tEmail}')
     
     # merge the email address with the alias
-    iEmail = f'{tEmail.split("@")[0]}+{alias}@{tEmail.split("@")[1]}'
+    if "@" in tEmail:
+        iEmail = f'{tEmail.split("@")[0]}+{alias}@{tEmail.split("@")[1]}'
+    else:
+        print("Invalid email address:", tEmail)
+        
     wMail.send_keys(iEmail)
-    tempEmailCounter += 1
     
+    # click the checkbox & submit
+    wTeach.click()
+    wSubmit.click()
+    
+    # check if the form was submitted successfully
+    try:
+        success_message = web.find_element(By.XPATH, "//p[text()='The form was submitted successfully.']")
+        print("\033[92mForm submitted successfully!\033[0m")
+        
+        sucSub += 1
+        
+        # go back to the form
+        web.back()
+        web.refresh()
+    except:
+        print("Form submission failed.")
+    
+    tempEmailCounter += 1
+
+print("========================================")
+print(f"Successfully submitted {sucSub} forms out of {num}.")
+print("========================================")
+
+time.sleep(5)
+
 # exit
 webEmail.quit()
 web.quit()
